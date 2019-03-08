bc0981e43f44d1373f9e9745d06ee13b7a6e5d2f
Jony Tucci <jonytucci01@gmail.com>
2019-03-08 14:43:10 -0800

    Creates  search result model that conforms to codable protocol

---
+++ AppStore/Models/SearchResult.swift
@@ -0,0 +1,22 @@
+//
+//  SearchResult.swift
+//  AppStore
+//
+//  Created by Jony Tucci on 3/8/19.
+//  Copyright © 2019 Jony Tucci. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+	
+	let resultCount: Int
+	let results: [Result]
+
+}
+
+struct Result: Codable {
+	let trackName: String
+	let primaryGenreName: String
+}
+
