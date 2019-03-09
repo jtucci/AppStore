1ba934190a138f193027d8308fd0e1e78a167c17
Jony Tucci <jonytucci01@gmail.com>
2019-03-09 10:15:31 -0800

    Creates AppsGroupCell with basic stub layout
    	- AppsController now uses AppsGroupCell instead of default UICollectionView cell

--- AppStore/Controllers/Apps/AppsController.swift
+++ AppStore/Controllers/Apps/AppsController.swift
@@ -11,11 +11,36 @@ import UIKit
 class AppsController: BaseCollectionViewController {
 	
 	//MARK:- Properties
+	let appSectionCellId = "AppSectionCell"
+	
 	
 	//MARK: - Life Cycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		collectionView?.backgroundColor = . yellow
+		
+		collectionView?.register(AppsGroupCell.self, forCellWithReuseIdentifier: appSectionCellId)
+	}
+	
+	//MARK:- Collection View Delegate
+	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 5
+	}
+	
+	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: appSectionCellId, for: indexPath)
+		return cell
+	}
+	
+}
+
+//MARK:- Collection View Flow Layout Delegate
+extension AppsController: UICollectionViewDelegateFlowLayout {
+	
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		
+		return CGSize(width: view.frame.width, height: 250)
+		
 	}
 	
 }
---
+++ AppStore/Views/Cells/Apps/AppsGroupCell.swift
@@ -0,0 +1,52 @@
+//
+//  AppsGroupCell.swift
+//  AppStore
+//
+//  Created by Jony Tucci on 3/9/19.
+//  Copyright © 2019 Jony Tucci. All rights reserved.
+//
+
+import UIKit
+
+class AppsGroupCell: UICollectionViewCell {
+	
+	
+	//MARK:- Properties
+	let titleLabel = UILabel(text: "Add Section", font: .boldSystemFont(ofSize: 30))
+
+	let horizontalController = UIViewController()
+	
+	//MARK:- Initialization
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		 backgroundColor = .lightGray
+		
+		
+		setupCellLayout()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	//MARK:- Setup
+	private func setupCellLayout() {
+		
+		
+		addSubview(titleLabel)
+		titleLabel.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor)
+		addSubview(horizontalController.view)
+		horizontalController.view.backgroundColor = .blue 
+		horizontalController.view.anchor(top: titleLabel.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+	}
+}
+
+extension UILabel {
+	
+	convenience init(text: String, font: UIFont) {
+		self.init(frame: .zero)
+		self.text = text
+		self.font = font
+	}
+	
+}
