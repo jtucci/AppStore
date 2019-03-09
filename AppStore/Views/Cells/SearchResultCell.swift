06455318506af8eb5687592889a9964d48226822
Jony Tucci <jonytucci01@gmail.com>
2019-03-08 19:17:44 -0800

    Removes setup code used for stubs

--- AppStore/Views/Cells/SearchResultCell.swift
+++ AppStore/Views/Cells/SearchResultCell.swift
@@ -33,8 +33,6 @@ class SearchResultCell: UICollectionViewCell {
 	
 	let appIconImageView: UIImageView = {
 		let iv = UIImageView()
-		iv.backgroundColor = .red
-		
 		iv.widthAnchor.constraint(equalToConstant: 64).isActive = true
 		iv.heightAnchor.constraint(equalToConstant: 64).isActive = true
 		iv.layer.cornerRadius = 12
@@ -130,7 +128,6 @@ class SearchResultCell: UICollectionViewCell {
 	//MARK:- Helper
 	private func createScreenShotImageView() -> UIImageView {
 		let imageView = UIImageView()
-		imageView.backgroundColor = .purple
 		imageView.layer.cornerRadius = 8
 		imageView.clipsToBounds = true
 		imageView.layer.borderWidth = 0.5
