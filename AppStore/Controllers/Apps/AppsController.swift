9a7489153c273f343d3532081a974aba01e2bee2
Jony Tucci <jonytucci01@gmail.com>
2019-03-09 09:34:02 -0800

    Creates AppsController which acts as the main controller for the apps tab

---
+++ AppStore/Controllers/Apps/AppsController.swift
@@ -0,0 +1,21 @@
+//
+//  AppsController.swift
+//  AppStore
+//
+//  Created by Jony Tucci on 3/9/19.
+//  Copyright © 2019 Jony Tucci. All rights reserved.
+//
+
+import UIKit
+
+class AppsController: BaseCollectionViewController {
+	
+	//MARK:- Properties
+	
+	//MARK: - Life Cycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		collectionView?.backgroundColor = . yellow
+	}
+	
+}
